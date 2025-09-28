08543838be3e3aba3fe2b6203919b76f34d43554
brawaga <brawaga@gmail.com>
2025-09-28 11:17:41 +0700

    Autoform not tested.

--- opinions_app/forms.py
+++ opinions_app/forms.py
@@ -1,24 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
+from wtforms_alchemy import ModelForm
 
-class OpinionForm(FlaskForm):
-    title = StringField(
-        'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 128)]
-    )
-    added_by = StringField(
-        'Автор',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 64)]
-    )
-    text = TextAreaField(
-        'Напишите мнение',
-        validators=[DataRequired(message='Обязательное поле')]
-    )
-    source = URLField(
-        'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()]
-    )
-    submit = SubmitField('Добавить')
+from opinions_app.models import Opinion
+
+
+# class OpinionForm(FlaskForm):
+#     title = StringField(
+#         'Введите название фильма',
+#         validators=[DataRequired(message='Обязательное поле'),
+#                     Length(1, 128)]
+#     )
+#     added_by = StringField(
+#         'Автор',
+#         validators=[DataRequired(message='Обязательное поле'),
+#                     Length(1, 64)]
+#     )
+#     text = TextAreaField(
+#         'Напишите мнение',
+#         validators=[DataRequired(message='Обязательное поле')]
+#     )
+#     source = URLField(
+#         'Добавьте ссылку на подробный обзор фильма',
+#         validators=[Length(1, 256), Optional()]
+#     )
+#     submit = SubmitField('Добавить')
+
+
+class OpinionForm(ModelForm):
+	class Meta:
+		model = Opinion
+		exclude = ['id', 'timestamp']
+
